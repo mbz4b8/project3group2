1ce5028d895dd7969c752bf858e83f0844347233
mbz4b8 <149974307+mbz4b8@users.noreply.github.com>
2024-03-13 22:36:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//read in the Spotify SQLite data from tables_json.json file
-const Spotifydata = 'tablesz_json.json";
+// Read in the Spotify SQLite data from tables_json.json file
+const Spotifydata = 'tablesz_json.json';
 
-//Fetch data and log to console
+// Fetch data and log to console
 d3.json(Spotifydata)
   .then(function(data) {
     console.log(data);
+  });
     
