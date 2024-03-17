645efa905d6ab3c02525ab443353492fa7cf0acd
mbz4b8 <149974307+mbz4b8@users.noreply.github.com>
2024-03-17 09:57:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Read in the Spotify SQLite data from tables_json.json file
+const Spotifydata = 'tables_data.json';
+
 // Fetch data and log to console
 fetch(Spotifydata)
   .then(response => response.json())
