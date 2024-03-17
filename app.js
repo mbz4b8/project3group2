073aeba0e2485ef440943216f0ba11c24eb464cd
mbz4b8 <149974307+mbz4b8@users.noreply.github.com>
2024-03-17 09:52:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// Read in the Spotify SQLite data from tables_json.json file
-const Spotifydata = 'tables_data.json';
-
-// Import data directly
-const data = require('./tables_data.json');
+// Fetch data and log to console
+fetch(Spotifydata)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
 
 // Assuming recentlyPlayed data is in data.recentlyPlayed
 const recentlyPlayed = data.recentlyPlayed;
