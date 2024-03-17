d14640d8902b82a49d8b9b54d70d2a7e37461d8e
mbz4b8 <149974307+mbz4b8@users.noreply.github.com>
2024-03-17 09:50:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 // Read in the Spotify SQLite data from tables_json.json file
 const Spotifydata = 'tables_data.json';
 
-// Fetch data and log to console
-fetch(Spotifydata)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+// Import data directly
+const data = require('./tables_data.json');
 
-// Recently Played Chart
-  // Bar chart data
-  let barData = [{
-    x: recentlyPlayed.rplTrackArtist,
-    y: recentlyPlayed.rplTrackPopularity,
-    type: 'bar',
-    text: recentlyPlayed.rplTrackName
-  }];
+// Assuming recentlyPlayed data is in data.recentlyPlayed
+const recentlyPlayed = data.recentlyPlayed;
 
-  // Bar chart layout
-  let barLayout = {
-    title: 'Spotify Artists Recently Played',
-    xaxis: {
-      title: 'Artist Name'
-    yaxis: {
-      title: 'Spotify Track Popularity'
-    }
-    }
-  };
+// Bar chart data
+let barData = [{
+  x: recentlyPlayed.map(entry => entry.rplTrackArtist),
+  y: recentlyPlayed.map(entry => entry.rplTrackPopularity),
+  type: 'bar',
+  text: recentlyPlayed.map(entry => entry.rplTrackName)
+}];
 
-  // Plot the bar chart
-  Plotly.newPlot("bar", barData, barLayout);
+// Bar chart layout
+let barLayout = {
+  title: 'Spotify Artists Recently Played',
+  xaxis: {
+    title: 'Artist Name'
+  },
+  yaxis: {
+    title: 'Spotify Track Popularity'
+  }
+};
+
+// Plot the bar chart
+Plotly.newPlot("bar", barData, barLayout);
